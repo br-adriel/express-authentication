d8cac5e2b882e66a54ea27c923cf25bc86c4473a
Adriel <adriel.fariasan@gmail.com>
2022-07-24 20:57:51 -0300

    Adiciona primeira rota com o express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world')
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'bar' });
+});
+
+app.listen(3000, () =>
+  console.log(`Servidor em execução => http://localhost:3000`)
+);
