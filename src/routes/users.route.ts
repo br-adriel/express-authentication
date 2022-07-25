d45f051317e72f9de7f92be4d854eef1b2991d67
Adriel <adriel.fariasan@gmail.com>
2022-07-24 22:04:16 -0300

    Implementa rota get de usuario especifico

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,19 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import statusCodes from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'Adriel' }];
-  res.status(200).send(users);
+  res.status(statusCodes.OK).send(users);
 });
+
+usersRoute.get(
+  '/users/:uuid',
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(statusCodes.OK).send(uuid);
+  }
+);
 
 export default usersRoute;
