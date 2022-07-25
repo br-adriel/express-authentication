701f7c4afad806a0758f579e750074a6bdb4940a
Adriel <adriel.fariasan@gmail.com>
2022-07-24 23:52:58 -0300

    Implementa funcao para buscar usuarios no bd

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,13 @@
+import db from '../db';
+import User from '../models/users.model';
+
+class UserRepository {
+  async findAllUsers(): Promise<User[]> {
+    const query = `SELECT uuid, username FROM application_user`;
+    const result = await db.query<User>(query);
+    const rows = result.rows;
+    return rows || [];
+  }
+}
+
+export default new UserRepository();
