a6e345e74ad97642bbee9db1c1703cd03a5cad7d
Adriel <adriel.fariasan@gmail.com>
2022-07-24 22:16:39 -0300

    Implementa rota post de criacao de usuario

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import usersRoute from './routes/users.route';
 
 const app = express();
 
+// Configuracoes da aplicacao
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configuracoes das rotas
 app.use(usersRoute);
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: 'bar' });
 });
 
+// Inicializacao do servidor
 app.listen(3000, () =>
   console.log(`Servidor em execução => http://localhost:3000`)
 );
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -16,4 +16,9 @@ usersRoute.get(
   }
 );
 
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  res.status(statusCodes.CREATED).send(newUser);
+});
+
 export default usersRoute;
